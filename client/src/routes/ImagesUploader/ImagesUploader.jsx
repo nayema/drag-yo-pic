e2347f0301ab9a00ecf5023afba70aa8c04cfdc0
Nayema Nasir <nayema.nasir89@gmail.com>
2018-01-16 22:10:25 -0500

    Adds the ability to sort photos

--- client/src/routes/ImagesUploader/ImagesUploader.jsx
+++ client/src/routes/ImagesUploader/ImagesUploader.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import request from 'superagent'
+import Gallery from 'react-photo-gallery'
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc'
 import { withStyles } from 'material-ui/styles'
+
+import Photo from './Photo'
 
 const styles = (theme) => ({
   root: {
@@ -13,12 +17,18 @@ const styles = (theme) => ({
   }
 })
 
+const SortablePhoto = SortableElement(Photo)
+const SortableGallery = SortableContainer(({ photos }) => (
+  <Gallery photos={photos} ImageComponent={SortablePhoto}/>
+))
+
 class ImagesUploader extends React.Component {
   constructor (props) {
     super(props)
     this.state = { photos: [] }
 
     this.onDrop = this.onDrop.bind(this)
+    this.onSortEnd = this.onSortEnd.bind(this)
   }
 
   onDrop (files) {
@@ -30,7 +40,11 @@ class ImagesUploader extends React.Component {
         if (err) console.log(err)
         this.setState(prevState => {
           const oldPhotos = prevState.photos
-          const newPhotos = res.body.files.map(file => `uploads/${file.filename}`)
+          const newPhotos = res.body.files.map(file => ({
+            src: `uploads/${file.filename}`,
+            width: 1,
+            height: 1
+          }))
           return {
             photos: oldPhotos.concat(newPhotos)
           }
@@ -38,13 +52,17 @@ class ImagesUploader extends React.Component {
       })
   }
 
+  onSortEnd ({ oldIndex, newIndex }) {
+    this.setState({
+      photos: arrayMove(this.state.photos, oldIndex, newIndex)
+    })
+  }
+
   render () {
     return (
       <Dropzone style={{}} disableClick={true} onDrop={this.onDrop} accept={'image/*'}>
         <div>Upload your pictures here</div>
-        {this.state.photos.map((photo) => (
-          <img key={photo} src={photo} width={200}/>
-        ))}
+        <SortableGallery axis={'xy'} photos={this.state.photos} onSortEnd={this.onSortEnd}/>
       </Dropzone>
     )
   }
